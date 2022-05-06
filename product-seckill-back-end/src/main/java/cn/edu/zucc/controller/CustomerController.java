294299eea79b691b30091319cacf6f2ec75626ad
myst5th <1192445074@qq.com>
2022-05-06 15:08:38 +0800

    修改了状态码常量

--- product-seckill-back-end/src/main/java/cn/edu/zucc/controller/CustomerController.java
+++ product-seckill-back-end/src/main/java/cn/edu/zucc/controller/CustomerController.java
@@ -2,7 +2,6 @@ package cn.edu.zucc.controller;
 
 import cn.edu.zucc.domain.Customer;
 import cn.edu.zucc.service.ICustomerService;
-import cn.edu.zucc.utils.Constant;
 import cn.edu.zucc.utils.JwtUtil;
 import cn.edu.zucc.utils.Result;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -10,6 +9,8 @@ import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import static cn.edu.zucc.utils.Constant.*;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -26,10 +27,10 @@ public class CustomerController {
         if (token != null) {
             Long exp = JwtUtil.getExpiration(token);
             if (exp != null && exp > System.currentTimeMillis()) {
-                return Result.ok(Constant.AUTHORIZED, "用户匹配成功");
+                return Result.ok(SUCCESS, "用户匹配成功");
             }
         }
-        return Result.fail(Constant.UNAUTHORIZED, "登录已过期，请重新登录");
+        return Result.fail(UNAUTHORIZED, "登录已过期，请重新登录");
     }
 
     @GetMapping("/{account}/{password}")
@@ -39,9 +40,17 @@ public class CustomerController {
         Customer one = customerService.getOne(wrapper);
         if (one != null && one.getPassword().equals(password)) {
             String token = JwtUtil.createToken(one.getId(), account, password);
-            return Result.ok(token, 200, "登陆成功");
+            return Result.ok(token, SUCCESS, "登陆成功");
         }
-        return Result.fail(400);
+        return Result.fail(FAIL,"登陆失败，用户名或密码错误");
     }
 
+    @PostMapping
+    public Result signUp(@RequestBody Customer customer) {
+        boolean save = customerService.save(customer);
+        if (save) {
+            return Result.ok(SUCCESS,"注册成功");
+        }
+        return Result.fail(SIGNUP_FAIL,"服务器出错");
+    }
 }
--- product-seckill-back-end/src/main/java/cn/edu/zucc/utils/Constant.java
+++ product-seckill-back-end/src/main/java/cn/edu/zucc/utils/Constant.java
@@ -2,5 +2,7 @@ package cn.edu.zucc.utils;
 
 public class Constant {
     public static final Integer UNAUTHORIZED = 401;
-    public static final Integer AUTHORIZED = 401;
+    public static final Integer SIGNUP_FAIL = 500;
+    public static final Integer SUCCESS = 200;
+    public static final Integer FAIL = 400;
 }
